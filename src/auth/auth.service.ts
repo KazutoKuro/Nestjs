4d8f66c6067a07a55bf0e29171041cf9d37c3900
KauztoKuro <ikkikurogane70@gmail.com>
2021-12-30 16:23:49 +0630

    L-5.4: Validation Credentials and Password Strength

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { AuthCredentialsDto } from './dto/auth.credentials.dto';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './users.repository';
 
 @Injectable()
