66c31c6e04d5a791b2a282ef3d306eabb1b60691
KauztoKuro <ikkikurogane70@gmail.com>
2022-01-02 10:44:27 +0630

    L-5.5 Error Handling User Conflits

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,3 +1,4 @@
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
@@ -7,6 +8,17 @@ export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
     const user = this.create({ username, password });
-    await this.save(user);
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        // duplicate username
+        throw new ConflictException('Username already existss');
+      } else {
+        throw new InternalServerErrorException();
+      }
+      console.error(error.code);
+    }
   }
 }
