cb035b317f5494786ea85e498f4e9aa5c1aa7d27
KauztoKuro <ikkikurogane70@gmail.com>
2022-01-03 01:43:09 +0630

    L-5.14: Guarding the Tasks Routes

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,6 +7,7 @@ import {
   Post,
   Patch,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './task-status.enum';
@@ -14,8 +15,10 @@ import { TasksService } from './tasks.service';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { Task } from './task.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
