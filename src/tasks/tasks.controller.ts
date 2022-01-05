d052bc81f408622345a6cd5f4c1cc7adda228682
KauztoKuro <ikkikurogane70@gmail.com>
2022-01-05 14:30:58 +0630

    L-8.2: Implementing Logs in our NestJS application

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -18,10 +18,13 @@ import { Task } from './task.entity';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from 'src/auth/get-user.decorator';
 import { User } from 'src/auth/user.entity';
+import { Logger } from '@nestjs/common';
 
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
+  private logger = new Logger('TasksController');
+
   constructor(private tasksService: TasksService) {}
 
   // //http://localhost:3000/tasks
@@ -81,6 +84,11 @@ export class TasksController {
     @Query() filterDto: GetTasksFilterDto,
     @GetUser() user: User,
   ): Promise<Task[]> {
+    this.logger.verbose(
+      `User "${user.username}" retrieving all tasks. Filters: ${JSON.stringify(
+        filterDto,
+      )}`,
+    );
     return this.tasksService.getTasks(filterDto, user);
   }
 
@@ -94,6 +102,9 @@ export class TasksController {
     @Body() createTaskDto: CreateTaskDto,
     @GetUser() user: User,
   ): Promise<Task> {
+    this.logger.verbose(
+      `User "${user.username}" creating a new task. Data: ${JSON.stringify(createTaskDto,)}`,
+    );
     return this.tasksService.createTask(createTaskDto, user);
   }
 
